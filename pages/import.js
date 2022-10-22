ed0f87eb404ee048c9698260bd4df5c7eb8ff20a
natthamol44 <natthamol.46@mail.kmutt.ac.th>
2022-10-22 16:02:13 +0700

    Background

--- pages/import.js
+++ pages/import.js
@@ -4,7 +4,7 @@ export default function Import() {
     return (
         <>
             <div className="w-full h-screen bg-black">
-                <div className=" bg-cover bg-center w-full h-full" style={{ backgroundImage: `url(/bg-import.png)` }}>
+                <div className="bg-center w-full h-full bg-no-repeat" style={{ backgroundImage: `url(/bg-import.png)` }}>
                     <p></p>
                 </div>
             </div>
