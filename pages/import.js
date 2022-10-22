95ac3cdd22f33978f05a851f944bf1b0403314e1
natthamol44 <natthamol.46@mail.kmutt.ac.th>
2022-10-22 15:07:28 +0700

    Background

---
+++ pages/import.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Import() {
+    return (
+        <>
+            <div className="w-full h-screen ">
+                <div className=" bg-cover bg-center w-full h-full" style={{ backgroundImage: `url(/bg-import.png)` }}>
+                    <p></p>
+                </div>
+            </div>
+        </>
+    )
+}
+
