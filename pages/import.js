590d061a438798c00418078eca72fe0c605f366f
natthamol44 <natthamol.46@mail.kmutt.ac.th>
2022-10-22 15:46:57 +0700

    Background

--- pages/import.js
+++ pages/import.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Import() {
     return (
         <>
-            <div className="w-full h-screen ">
+            <div className="w-full h-screen bg-black">
                 <div className=" bg-cover bg-center w-full h-full" style={{ backgroundImage: `url(/bg-import.png)` }}>
                     <p></p>
                 </div>
