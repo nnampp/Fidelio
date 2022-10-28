ebc347d0ade063258655f53e3ed8ea51559dbe60
Sarunny <sarunsarunny.new@gmail.com>
2022-10-28 13:41:40 +0700

    create listen to song

---
+++ pages/listentosong.js
@@ -0,0 +1,10 @@
+export default function listentosong() {
+    return (
+       <>
+          <div className="bg-[#131318] w-full h-screen "></div>
+          <div class="basis-3/5 bg-[#D9D9D9] rounded-r-[10px]"></div>
+
+
+       </>
+    )
+ }
