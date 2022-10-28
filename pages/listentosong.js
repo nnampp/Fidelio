b9997887e13868ab1406a1f1984767ddf485ac78
PJ-Meaw <pathinya19@gmail.com>
2022-10-28 14:53:47 +0700

    First listentosong for New

--- pages/listentosong.js
+++ pages/listentosong.js
@@ -1,10 +1,17 @@
+import music from "../public/music-playing.gif"
+
 export default function listentosong() {
-    return (
-       <>
-          <div className="bg-[#131318] w-full h-screen "></div>
-          <div class="basis-3/5 bg-[#D9D9D9] rounded-r-[10px]"></div>
-
-
-       </>
-    )
- }
+   return (
+      <>
+         <div className="bg-[#131318] w-full h-screen">
+            <div className="max-w-screen-xl mx-auto">
+               <div className="flex flex-row items-center h-screen justify-center">
+                  <div className="w-[1221px] h-[652px] rounded-[40px]" style={{ backgroundColor: 'rgba(187, 187, 187, 0.1)' }}>
+                     <img src={music.src} alt="" className="w-[621px] h-[576px]"/>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </>
+   )
+}
