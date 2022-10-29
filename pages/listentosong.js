57f259d8d69df1242de22a06ab9e2703c0535017
Sarunny <sarunsarunny.new@gmail.com>
2022-10-29 20:10:05 +0700

    listen to song finish(Maybe)

--- pages/listentosong.js
+++ pages/listentosong.js
@@ -7,11 +7,44 @@ export default function listentosong() {
             <div className="max-w-screen-xl mx-auto">
                <div className="flex flex-row items-center h-screen justify-center">
                   <div className="w-[1221px] h-[652px] rounded-[40px]" style={{ backgroundColor: 'rgba(187, 187, 187, 0.1)' }}>
-                     <img src={music.src} alt="" className="w-[621px] h-[576px]"/>
+
+                     <div className="flex flex-row">
+                        <img src={music.src} alt="" className="w-[621px] h-[576px] rounded-[30px] ml-9 mt-9" />
+
+                        <div className="flex flex-col">
+                           
+                           <div className="flex flex-row">
+                              <img src="Group 11.jpg" alt="" className=" rounded-[10px] w-[124px] h-[112px] ml-[111px] mt-[172px] " />
+                              <div className="flex flex-col">
+                                 <p className="text-[40px] font-Commissioner font-bold text-[#FFFFFF] ml-[38px] mt-[175px] ">Song Name</p>
+                                 <p className="text-[24px] font-Commissioner font-Semibold text-[#FFFFFF]  ml-[38px] mt-[5px] ">Artist</p>
+                                 
+                              </div>
+                           </div>
+                           <div className="flex flex-row">
+                           <img src="before.png" alt="" className=" w-[49px] h-[40px] ml-[173px] mt-[116px] " />
+                           <img src="Pause.png" alt="" className=" w-[62px] h-[62px] ml-[36px] mt-[105px] " />
+                           <img src="after.png" alt="" className=" w-[49px] h-[40px] ml-[36px] mt-[116px] " />
+                           </div>
+                           <div className="flex flex-row">
+                           <p className="text-[13px] font-Commissioner font-Semibold text-[#FFFFFF] ml-[38px] mt-[44px] ">01.50</p>
+                           <div></div>
+                           <p className="text-[13px] font-Commissioner font-Semibold text-[#FFFFFF] ml-[432px] mt-[44px] ">03.50</p>
+                           </div>
+                        </div>
+                     </div>
+
+
                   </div>
                </div>
+
+
+
             </div>
          </div>
+
+
       </>
    )
 }
+
