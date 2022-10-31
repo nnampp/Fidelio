35c97116fd57908d66ca0e7d74b4d185815ceba4
nnampp <hoei.p96m@gmail.com>
2022-10-31 07:38:46 +0700

    Display all song from firebase

--- pages/home.js
+++ pages/home.js
@@ -1,15 +1,39 @@
 import Navbar from "../components/Navbar"
 import Music from "../components/Music";
+import { useState, useEffect } from "react";
 const test = [
    {
-      song_name: "I love you 3000",
-      artist: "someone",
-      time: "3:46"
+      NameSong: "I love you 3000",
+      ArtistName: "someone",
+      Time: "3:46"
    },
 
 ];
 
 export default function home() {
+   const [song, getSong] = useState();
+
+   useEffect(() => {
+      async function getAllSonglist() {
+         const res = await fetch('/api/song', {
+         method: 'POST',
+         headers: { 'Content-Type': 'application/json' }
+      });
+      const res2 = await res.json();
+      const songAll = await Object.values(res2.song);
+      await getSong(songAll);
+      }
+      getAllSonglist();
+   }, []);
+
+   const showSong = () => {
+      return (
+         song?.map((num, i) => {
+            return <Music name={num.NameSong} artist={num.ArtistName} time={num.Time} key={i} />
+         })
+      )
+   }
+
    return (
       <>
          <div className="bg-[#2D106A] h-screen overflow-hidden">
@@ -17,6 +41,7 @@ export default function home() {
                <div className="w-full h-screen bg-gradient-to-b from-[#000000] via-[#100526] to-[#1C0943] rounded-[10px]">
                   <Navbar />
                   <div className="overflow-y-scroll h-screen mt-[101px] scrollbar scrollbar-width-2"> {/* part Song */}
+                     { showSong() }
                      <Music/>
                      <Music/>
                      <Music/>
