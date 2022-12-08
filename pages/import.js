dfa17a582bb4eeae5848e5c91322d3ecb6019772
nnampp <hoei.p96m@gmail.com>
2022-12-09 03:21:34 +0700

    Change flask API link

--- pages/import.js
+++ pages/import.js
@@ -21,7 +21,7 @@ export default function Import() {
          Role: "Admin"
       };
 
-      const res = await fetch('http://127.0.0.1:3000/reciever', {
+      const res = await fetch('https://fidelio-python-2xdoferf4a-as.a.run.app/', {
          method: 'POST',
          headers: { 'Content-Type': 'application/json' },
          body: JSON.stringify(body)
